5675440c4e23d2bdad1a321749d803728419448e
itsme-swe <me.harshmehra07@gmail.com>
2025-03-31 16:42:17 +0530

    update

---
+++ 06_Strings/2_AccessingString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string msg = "Welcome";
+
+  cout << msg[2] << endl; // output: l --- 🔸 accessing 2 index position of string
+
+  cout << msg << endl;  // output ⇒ Welcome
+
+  return 0;
+}
---
+++ 06_Strings/3_UpdatingString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string str = "Jello Welcome";
+
+  str[0] = 'H'; // updating first character of string
+
+  cout << str << endl; // 🔸 output ⇨ Hello Welcome
+
+  return 0;
+}
