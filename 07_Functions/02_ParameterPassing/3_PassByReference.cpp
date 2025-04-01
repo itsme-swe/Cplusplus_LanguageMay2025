76c4b71532ea2aa45a2ca4632a0ae87493161f14
itsme-swe <me.harshmehra07@gmail.com>
2025-04-01 06:48:04 +0530

    update

---
+++ 07_Functions/02_ParameterPassing/3_PassByReference.cpp
@@ -0,0 +1,29 @@
+/*
+💥 In pass-by-reference method, instead of passing the value of the argument, we pass the reference of an argument to the function. This allows the function to change the value of the original argument. This is useful when you have to pass large size data.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void swap(int &a, int &b)
+{
+  int temp = a;
+  a = b;
+  b = temp;
+}
+
+int main()
+{
+
+  int a = 10;
+  int b = 20;
+
+  swap(a, b);
+
+  cout << "a: " << a << endl; // 20
+
+  cout << "b: " << b << endl; // 10
+
+  return 0;
+}
